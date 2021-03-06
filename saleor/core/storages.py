0b102381a60d885dbd56a056721c87535db5e2f9
darknight <giovanni.piccinelli@icloud.com>
2021-03-06 10:37:51 +0100

    add overwrite image test

--- saleor/core/storages.py
+++ saleor/core/storages.py
@@ -7,6 +7,7 @@ class S3MediaStorage(S3Boto3Storage):
     def __init__(self, *args, **kwargs):
         self.bucket_name = settings.AWS_MEDIA_BUCKET_NAME
         self.custom_domain = settings.AWS_MEDIA_CUSTOM_DOMAIN
+        self.file_overwrite = settings.AWS_S3_FILE_OVERWRITE
         super().__init__(*args, **kwargs)
 
 
